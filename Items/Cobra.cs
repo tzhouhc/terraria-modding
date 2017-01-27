a09ced2be7fff028c2cbd0f0ed37bff8e2b026f7
Ting Zhou <tzhouhc@gmail.com>
2017-01-27 11:49:41 -0800

    added draft for Boneshredder and Cobra; updated graphics a bit

---
+++ Items/Cobra.cs
@@ -0,0 +1,62 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Borderlands.Items
+{
+	public class Cobra : ModItem
+	{
+		/*
+				The Infinity is a Vladof legendary pistol.
+				It does not consume ammo. And it shoots in a sine pattern.
+		*/
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.Handgun);
+			item.name = "Cobra";
+			item.damage = 135;
+			item.autoReuse = false;
+			item.toolTip = "Found out about this I was like DAAAMN,";
+			item.toolTip2 = "I'm bringing that gun BACK!";
+			item.useTime = 42;
+			item.useAnimation = 42;
+			item.value = 300000;
+			item.rare = 8;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SniperRifle, 1);
+			recipe.AddIngredient(ItemID.RocketLauncher, 1);
+			recipe.AddTile(TileID.MythrilAnvil);  // careful preparation
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			// Alternate simple recipe
+			ModRecipe recipe2 = new ModRecipe(mod);
+			recipe2.AddIngredient(ItemID.DirtBlock, 10);
+			recipe2.AddTile(TileID.WorkBenches);
+			recipe2.SetResult(this);
+			recipe2.AddRecipe();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 25f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			if (type == ProjectileID.BulletHighVelocity)
+			{
+				speedX *= 1.41f;
+				speedY *= 1.41f;
+			}
+			type = ProjectileId.ExplosiveBullet; // careful not to shoot your hands out!
+			return true;
+		}
+	}
+}
